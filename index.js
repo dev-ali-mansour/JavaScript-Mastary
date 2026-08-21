7a279bc37e4345bee8b325cadad1b20e95b9d098
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-21 10:00:19 +0300

    feat: enhance index.js with additional comments and documentation

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// This is a single line comment
+
+/* 
+This is a multi-line comment
+You can write comments in your code to explain what it does 
+or to leave notes for yourself or other developers. 
+Comments are ignored by the JavaScript engine 
+and do not affect the execution of the code.
+*/
+
 console.log("Hello, World!!!");
 console.log("Hello, World!!!");
+
+/**
+ * Adds two numbers together and returns the result.
+ *
+ * @param {number} a - The first number to add
+ * @param {number} b - The second number to add
+ * @returns {number} The sum of a and b
+ */
+function add(a, b) {
+  return a + b;
+}
+
