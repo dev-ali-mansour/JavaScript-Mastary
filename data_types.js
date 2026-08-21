57a743360d7406d7d0cfb5ec6a0965d43fc0433b
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-21 11:36:48 +0300

    feat: add data_types.js for demonstrating primitive and reference data types

---
+++ data_types.js
@@ -0,0 +1,55 @@
+// Primitive Data Types
+// Number: represents numeric values, both integers and floating-point numbers.
+let age = 70; // integer
+console.log(typeof age); // Output: number
+console.log(age); // Output: 70
+
+// String: represents sequences of characters, enclosed in single or double quotes.
+let _name = "Alice";
+console.log(typeof _name); // Output: string
+console.log(_name); // Output: Alice
+
+let city = "San Jose";
+console.log(typeof city); // Output: string
+console.log(city); // Output: San Jose
+
+console.log(_name + " " + city); // Output: Alice San Jose
+
+// Boolean: represents a logical value that can be either true or false.
+let hasGraduated = true;
+console.log(typeof hasGraduated); // Output: boolean
+console.log(hasGraduated); // Output: true
+
+// Undefined: represents a variable that has been declared but has not been assigned a value. It is also a primitive value that indicates the absence of a value.
+let area;
+console.log(typeof area); // Output: undefined
+console.log(area); // Output: undefined
+
+// Null: represents the intentional absence of any object value. It is a primitive value that can be assigned to a variable to indicate that it has no value.
+let emptyValue = null;
+console.log(typeof emptyValue); // Output: object (this is a known quirk in JavaScript)
+console.log(emptyValue); // Output: null
+
+// Reference Data Types
+// Object: represents a collection of key-value pairs, where each key is a string and each value can be any data type.
+let user = {
+  name: "Alice",
+  age: 30,
+  city: "San Jose",
+};
+console.log(typeof user); // Output: object
+console.log(user); // Output: { name: 'Alice', age: 30, city: 'San Jose' }
+console.log(user.name); // Output: Alice
+console.log(user.age); // Output: 30
+user.name = "John";
+console.log(user.name); // Output: John
+
+// Array: represents an ordered collection of values, which can be of any data type. Arrays are zero-indexed, meaning that the first element has an index of 0.
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(typeof fruits); // Output: object
+console.log(fruits); // Output: [ 'Apple', 'Banana', 'Orange' ]
+console.log(fruits[0]); // Output: Apple
+console.log(fruits[1]); // Output: Banana
+console.log(fruits[2]); // Output: Orange
+fruits.push("Grapes");
+console.log(fruits); // Output: [ 'Apple', 'Banana', 'Orange', 'Grapes' ]   
